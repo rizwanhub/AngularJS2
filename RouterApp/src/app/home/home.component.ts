eac72924238072c43f16f1f95bd1ede09879f503
Rizwan Ahmed <rizwan.ahmed@live.com>
2017-03-29 20:28:32 +0500

    commit

--- RouterApp/src/app/home/home.component.ts
+++ RouterApp/src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Http, Response, RequestOptions, Headers } from '@angular/http';
 
 @Component({
   selector: 'app-home',
@@ -7,7 +8,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+
+data;
+  selectedData;
+  constructor(private http: Http){
+    this.http.request('http://jsonplaceholder.typicode.com/posts')
+      .subscribe((res: Response) => {
+        this.data = res.json();
+        console.log(this.data);
+      });
+      
+  }
+
+  clicked(id: number){
+     this.http.request('http://jsonplaceholder.typicode.com/posts/'+ id)
+      .subscribe((res: Response) => {
+        this.selectedData = res.json();
+        console.log(this.selectedData);
+      });
+  }
+
 
   ngOnInit() {
   }
